59e2eeb8412853d2309b9e8b06a4c25f9ca01085
sushma0918 <sushmamanapuram18@gmail.com>
2025-12-14 12:04:56 +0530

    Update tri.py

--- tri.py
+++ tri.py
@@ -1,5 +1,5 @@
 #24331A05G8
-#Area of circle
+#Area of Triangle
 b, h = map(float, input("Enter the base and height of the triangle (comma-separated): ").split(","))
 area = 0.5 * b * h
 print("Area of the triangle is:", area)
